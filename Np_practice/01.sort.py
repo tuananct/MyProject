d4bc5804933fe43ca1048f257fd6989422be0502
tuananct <tuananct@yahoo.co.jp>
2016-02-25 17:20:55 +0900

    Add practice Numpy

---
+++ Np_practice/01.sort.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 17:06:40 2016
+
+@author: ubuntu
+"""
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)/2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print quicksort(3, 10, 4, 1, 2, 6, 1, 7)
